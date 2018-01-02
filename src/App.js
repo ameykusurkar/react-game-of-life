6b00d7e52a1116ed2201b28069bda48d0a7e8aa3
Amey Kusurkar <amey1000@gmail.com>
2018-01-02 14:25:08 +0800

    Add ability to select cells

--- src/App.js
+++ src/App.js
@@ -11,15 +11,26 @@ class App extends React.Component {
     super();
     this.state = {
       generation: 0,
-      grid: Array(ROWS).fill(Array(COLUMNS).fill(false))
+      grid: Array(ROWS).fill(Array(COLUMNS).fill(false)),
+      gameIsRunning: false
     };
+  }
+
+  handleClick(i, j) {
+    if (this.state.gameIsRunning) { return; }
+
+    var newGrid = cloneGrid(this.state.grid);
+    console.log(i, j);
+    newGrid[i][j] = !newGrid[i][j];
+    this.setState({ grid: newGrid });
   }
 
   render() {
     return (
       <div className="App">
         <h1>{"Conway's Game of Life"}</h1>
-        <Board grid={this.state.grid}/>
+        <Board grid={this.state.grid}
+               handleClick={(i, j) => this.handleClick(i, j)}/>
         <p>Generations: {this.state.generation}</p>
       </div>
     );
@@ -27,6 +38,11 @@ class App extends React.Component {
 }
 
 class Board extends React.Component {
+  // We need this function to force i, j to be passed by value
+  createClickHandler(i, j) {
+    return () => this.props.handleClick(i, j);
+  }
+
   render() {
     var cells = [];
     const rows = this.props.grid.length;
@@ -35,9 +51,12 @@ class Board extends React.Component {
     for (var i = 0; i < rows; i++) {
       for (var j = 0; j < columns; j++) {
         var cellClass = this.props.grid[i][j] ? "Cell on" : "Cell off";
+        var cellKey = i + "_" + j;
         cells.push(<Cell height={CELL_WIDTH}
                          width={CELL_WIDTH}
-                         cellClass={cellClass} />);
+                         cellClass={cellClass}
+                         key={cellKey}
+                         onClick={this.createClickHandler(i, j)} />);
       }
     }
 
@@ -54,9 +73,18 @@ class Cell extends React.Component {
     return (
       <div className={this.props.cellClass}
            style={{width: this.props.width, height: this.props.height}}
+           onClick={this.props.onClick}
       />
     );
   }
+}
+
+function cloneGrid(grid) {
+  var newGrid = [];
+  for (var i = 0; i < grid.length; i++) {
+    newGrid[i] = grid[i].slice();
+  }
+  return newGrid;
 }
 
 export default App;
